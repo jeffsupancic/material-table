db681ad6cbaa525f475d74558f203ec005516e5f
radiylonmina <radiylon@gmail.com>
2021-04-12 14:43:47 -0700

    Add onDragEnd function from props

--- src/material-table.js
+++ src/material-table.js
@@ -501,6 +501,7 @@ export default class MaterialTable extends React.Component {
   onDragEnd = (result) => {
     console.log('onDragEnd FIRED');
     if (!result || !result.source || !result.destination) return;
+    this.props.onDragEnd();
     this.dataManager.changeByDrag(result);
     this.setState(this.dataManager.getRenderState(), () => {
       if (
